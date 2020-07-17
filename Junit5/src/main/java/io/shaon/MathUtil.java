827b224f8cabb80b949f2ddc7c3075f7b7706241
shaon <shaon@cokreates.com>
2020-07-17 15:00:43 +0600

    Basic project added

---
+++ Junit5/src/main/java/io/shaon/MathUtil.java
@@ -0,0 +1,9 @@
+package io.shaon;
+
+public class MathUtil {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+}
---
+++ Junit5/src/test/java/io/shaon/MathUtilTest.java
@@ -0,0 +1,13 @@
+package io.shaon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MathUtilTest {
+
+    @Test
+    void test(){
+        fail("Not implemented");
+    }
+}
